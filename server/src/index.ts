913a0a7ebc66432b51e05e8e438c3143c37c6616
NeuraSync <kefakameti@gmail.com>
2025-07-24 20:54:25 +0300

    modify cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-y-seven.vercel.app",
     credentials: true,
     
     // methods:["POST", "GET", "PUT", "PATCH", "DELETE"]
